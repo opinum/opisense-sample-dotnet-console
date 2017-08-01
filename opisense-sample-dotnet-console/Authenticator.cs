8dec5e28862035b729492a94c44ab0404b64eae6
Denis Bollinne <dbo@opinum.com>
2017-08-01 08:41:24 +0200

    support in proc authentication

--- opisense-sample-dotnet-console/Authenticator.cs
+++ opisense-sample-dotnet-console/Authenticator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Configuration;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using IdentityModel.Client;
 
@@ -14,6 +15,7 @@ namespace opisense_sample_dotnet_console
 
         private static readonly string Username = ConfigurationManager.AppSettings["OpisenseUsername"];
         private static readonly string Password = ConfigurationManager.AppSettings["OpisensePassword"];
+        private static readonly string AskForCredentials = ConfigurationManager.AppSettings["AskForCredentials"];
 
         /// <summary>
         /// Access token should be stored and use accross calls, and the refresh token should be used to review the Access token
@@ -21,9 +23,19 @@ namespace opisense_sample_dotnet_console
         /// <returns></returns>
         private async Task<string> GetBearerToken()
         {
+            var username = Username;
+            var password = Password;
+            if (!string.IsNullOrWhiteSpace(AskForCredentials) && bool.Parse(AskForCredentials))
+            {
+                Console.WriteLine("Please enter your Username:");
+                username = Console.ReadLine();
+                Console.WriteLine("Please enter your Password:");
+                password = GetConsolePassword();
+            }
+
             var scopes = new[] { "openid", "opisense-api", "push-data" };
             var response = await new TokenClient($"{IdentityServer}connect/token", ClientId, ClientSecret)
-                .RequestResourceOwnerPasswordAsync(Username, Password, string.Join(" ", scopes));
+                .RequestResourceOwnerPasswordAsync(username, password, string.Join(" ", scopes));
             if (response.IsError)
             {
                 throw new Exception(response.Error);
@@ -37,6 +49,36 @@ namespace opisense_sample_dotnet_console
             var client = new HttpClient();
             client.SetBearerToken(bearerToken);
             return client;
+        }
+
+        private static string GetConsolePassword()
+        {
+            StringBuilder sb = new StringBuilder();
+            while (true)
+            {
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+
+                if (cki.Key == ConsoleKey.Backspace)
+                {
+                    if (sb.Length > 0)
+                    {
+                        Console.Write("\b\0\b");
+                        sb.Length--;
+                    }
+
+                    continue;
+                }
+
+                Console.Write('*');
+                sb.Append(cki.KeyChar);
+            }
+
+            return sb.ToString();
         }
     }
 }
